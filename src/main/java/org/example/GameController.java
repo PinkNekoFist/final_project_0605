98d8dfe29832b03ab4cbc0aa9c16c7f47ab0d505
nacl <vitus.tseng@gmail.com>
2024-06-05 16:07:07 +0800

    init camera, terminal and world

--- src/main/java/org/example/GameController.java
+++ src/main/java/org/example/GameController.java
@@ -1,13 +1,42 @@
 package org.example;
 
-public class GameController {
-    private boolean running = false;
+import org.jline.terminal.Terminal;
 
-    public void start() {
+import java.io.IOException;
+
+public class GameController {
+
+    static boolean running = false;
+    static final double moveDistance = 1;
+    static final double rotateAngle = 0.1;
+
+    public static void main(String[] args) throws IOException {
+        World world = new World(30, 30);
+        Input input = new Input();
+        Output out = new Output();
+        Terminal terminal;
+        Camera camera = new Camera(15, 15, 0, 70, terminal.getWidth());
+
+        start();
+        while (running) {
+            if () {
+                camera.move(moveDistance);
+            }
+
+            if () {
+                camera.rotate(rotateAngle);
+            }
+
+            Graphic frame = new Graphic(terminal.getWidth(), terminal.getHeight(), camera);
+            out.output(frame);
+        }
+    }
+
+    public static void start() {
         running = true;
     }
 
-    public void stop() {
+    public static void stop() {
         running = false;
     }
 
