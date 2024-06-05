aac6fd0bd703933912869806de3bab9bb859c81a
nacl <vitus.tseng@gmail.com>
2024-06-05 16:06:23 +0800

    remove main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,14 +1,4 @@
 package org.example;
 
 public class Main {
-    public static void main(String[] args) {
-        GameController gameController = new GameController();
-        Input input = new Input();
-        Camera camera = new Camera(0, 0, 0);
-        gameController.start();
-
-        while (gameController.isRunning()) {
-
-        }
-    }
 }
