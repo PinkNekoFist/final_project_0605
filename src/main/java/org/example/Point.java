992f80078412e52144fa9669181deb9e01eb8caf
nacl <vitus.tseng@gmail.com>
2024-06-06 20:11:30 +0800

    add double point position

--- src/main/java/org/example/Point.java
+++ src/main/java/org/example/Point.java
@@ -1,10 +1,10 @@
 package org.example;
 
 public class Point {
-    public int x;
-    public int y;
+    public double x;
+    public double y;
 
-    public Point(int x, int y) {
+    public Point(double x, double y) {
         this.x = x;
         this.y = y;
     }
