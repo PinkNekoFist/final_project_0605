7e459caea70f01825ecbe17673f4bba247bc6b9e
nacl <vitus.tseng@gmail.com>
2024-06-08 04:09:56 +0800

    add renderDistance

--- src/main/java/org/example/Camera.java
+++ src/main/java/org/example/Camera.java
@@ -1,18 +1,28 @@
 package org.example;
 
-public class Camera {
+interface Movement {
+    public void move (double distance);
+}
+
+interface Rotation {
+    public void rotate (int angle);
+}
+
+public class Camera implements Movement, Rotation{
     double x;
     double y;
     double fov;
     int angle;
     double distanceFromScreen;
+    double renderDistance;
 
-    public Camera(double x, double y, int angle, double fov, int terminalWidth) {
+    public Camera(double x, double y, int angle, double fov, int terminalWidth, double renderDistance) {
         this.x = x;
         this.y = y;
         this.fov = fov; // 70
         this.distanceFromScreen = (((double)terminalWidth * 0.005 / 2) / Math.tan(Math.toRadians(fov / 2))); // about 0.7426
         this.angle = angle;
+        this.renderDistance = renderDistance;
         System.out.println("x: " + x + " y: " + y + " angle: " + angle + " fov: " + fov + " distanceFromScreen: " + distanceFromScreen);
     }
 
