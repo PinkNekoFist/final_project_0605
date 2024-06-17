9178840be701f8ddcc48ffde606549b38a415af5
nacl <vitus.tseng@gmail.com>
2024-06-17 18:31:46 +0800

    rewrite changeMap function

--- src/main/java/org/example/World.java
+++ src/main/java/org/example/World.java
@@ -5,6 +5,8 @@ public class World {
     private int height;
     private char[][] map;
     public World(int width, int height) {
+        this.width = width;
+        this.height = height;
         map = new char[height][width];
         for (int y = 0; y < height; y++) {
             for (int x = 0; x < width; x++) {
@@ -15,18 +17,15 @@ public class World {
                 }
             }
         }
-        for (int y = 20; y < 30; y++) {
-            for (int x = 20; x < 30; x++) {
-                map[y][x] = '#';
-            }
-        }
+        changeMap(20, 35, 20 ,35);
     }
 
-    public void changeMap(int x, int y) {
-        if (x < 0 || y < 0 || x >= width || y >= height) {
-            return;
-        } else {
-            map[y][x] = '#';
+    public void changeMap(int x1, int x2, int y1, int y2) {
+        if (x1 < 0 || y1 < 0 || x1 > x2 || y1 > y2 || x2 >= width || y2 >= height) return;
+        for (int i = x1; i <= x2; i++) {
+            for (int j = y1; j <= y2; j++) {
+                map[j][i] = '#';
+            }
         }
     }
 
