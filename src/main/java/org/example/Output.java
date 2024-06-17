0241fca02ae55ce62d507db308e9b91f480db858
nacl <vitus.tseng@gmail.com>
2024-06-17 14:15:57 +0800

    fix ansiPrint and interface of Print

--- src/main/java/org/example/Output.java
+++ src/main/java/org/example/Output.java
@@ -4,51 +4,50 @@ import org.jline.utils.AnsiWriter;
 import java.io.OutputStreamWriter;
 import java.io.IOException;
 
-public class Output {
-    private Graphic frameLast;
+interface Print {
+    public void output(Graphic graphic) throws IOException;
+    public void ansiPrint(int y, int x, char c) throws IOException;
+    public void printAllScreen(char[][] frame) throws IOException;
+}
+
+public class Output implements Print {
+    private char[][] frameLast;
 
     public Output () {
 
     }
 
-    public void output(Graphic frame) throws IOException {
-        AnsiWriter ansiWriter = new AnsiWriter(new OutputStreamWriter(System.out));
+    public void output(Graphic graphic) throws IOException {
+        char[][] frame = graphic.getFrame();
         if (frameLast == null) {
-            printAllScreen(ansiWriter, frame);
+            printAllScreen(frame);
             return;
         }
-        for (int i = 0; i < frame.height; i++ ) {
-            for (int j = 0; j < frame.width; j++ ) {
-                if (frame.frame[i][j] != frameLast.frame[i][j]) {
-                    ansiPrint(ansiWriter, i, j, frame.frame[i][j]);
+        for (int i = 0; i < frame.length; i++ ) {
+            for (int j = frame[0].length - 1; j >= 0 ; j-- ) {
+                if (frame[i][j] != frameLast[i][j]) {
+                    ansiPrint(i, j, frame[i][j]);
                 }
             }
         }
         frameLast = frame;
     }
 
-    public void ansiPrint(AnsiWriter ansiWriter, int y,int x, char c) throws IOException {
+    public void ansiPrint(int y,int x, char c) throws IOException {
         // Move the cursor to row y, column x
-        String temp = String.format("\033[%d;%dH", y, x);
-        ansiWriter.write(temp);
-        // write
-        ansiWriter.write(c);
-
-        // Reset to default attributes
-        ansiWriter.write("\033[0m");
-        ansiWriter.flush();
+        String temp = String.format("\033[%d;%dH", y+1, x+1);
+        System.out.print(temp);
+        System.out.print(c);
     }
 
-    public void printAllScreen (AnsiWriter ansiWriter, Graphic frame) throws IOException {
-        ansiWriter.write("\033[H");
-        for (int i = 0; i < frame.height; i++ ) {
-            for (int j = 0; j < frame.width; j++ ) {
-                System.out.print(frame.frame[i][j]);
+    public void printAllScreen (char[][] frame) throws IOException {
+        System.out.print("\033[H");
+        for (int i = 0; i < frame.length; i++ ) {
+            for (int j = frame[0].length - 1; j >= 0; j-- ) {
+                System.out.print(frame[i][j]);
             }
-            if (i != frame.height - 1 ) System.out.println();
+            if (i != frame.length - 1 ) System.out.println();
         }
         frameLast = frame;
-        ansiWriter.write("\033[0m");
-        ansiWriter.flush();
     }
 }
